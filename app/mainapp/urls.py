843b1206a55e26008fb957d1a53aa2672fe4f090
chamorcev <chamorcev@gmail.com>
2022-10-10 00:23:43 +0300

    Добавил страницу с пользовательского соглашения.

--- app/mainapp/urls.py
+++ app/mainapp/urls.py
@@ -7,4 +7,5 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('terms_of_service/', views.terms_of_service, name='terms_of_service')
 ]
--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
 
 class HomePageView(TemplateView):
     template_name = 'home_page.html'
@@ -6,3 +8,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+def terms_of_service(request):
+    return render(request, 'terms_of_service.html')
