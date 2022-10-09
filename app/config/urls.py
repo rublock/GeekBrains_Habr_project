88f9b9fc8b296147c6a2799378895a9c2a751c63
Vladimir <gromovvladimir@myrambler.ru>
2022-10-10 01:15:12 +0300

    autorization

--- app/config/urls.py
+++ app/config/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
+    path('users/', include('userapp.urls')),
 ]
 
 if settings.DEBUG:
