b50e983be42ef9e2105e1bc2d6aa31145212cebb
rateterin <rateterin@yandex.ru>
2022-10-10 09:37:44 +0300

    Добавлены настройки редактора Поправлен mainapp/admin.py Добавлен редактор в редактирование поста в админке

--- app/config/settings.py
+++ app/config/settings.py
@@ -114,3 +114,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'userapp.User'
+
+# django_ckeditor
+CKEDITOR_BASEPATH = '/static/ckeditor/ckeditor/'
+CKEDITOR_UPLOAD_PATH = 'uploads/'
