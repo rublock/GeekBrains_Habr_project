6d48b478fff5143f3e1c8192c30ecdbb03f0035c
Mack <hypermail@yandex.ru>
2022-10-10 20:59:35 +0300

    reg_page frontend

--- app/config/settings.py
+++ app/config/settings.py
@@ -85,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class HomePageView(TemplateView):
     template_name = 'home_page.html'
@@ -6,3 +7,6 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+def reg_page(request):
+    return render(request, 'reg_page.html')
