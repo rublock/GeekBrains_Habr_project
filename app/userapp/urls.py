ec29d24331d4939598f37bdaa0b4b1241607b83a
Vladimir <gromovvladimir@myrambler.ru>
2022-10-10 21:18:54 +0300

    Переименовал класс

--- app/userapp/urls.py
+++ app/userapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from userapp.views import Register
 
-
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', Register.as_view(), name='register'),
