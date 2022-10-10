a7a8b3b4eac0447e699623d73a1d463098815263
ram1k <zo0zo0@mail.ru>
2022-10-10 23:15:47 +0300

    created 404 page

--- app/mainapp/urls.py
+++ app/mainapp/urls.py
@@ -7,4 +7,5 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('page_404', views.Page404.as_view(), name='page_404')
 ]
--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -6,3 +6,11 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class Page404(TemplateView):
+    template_name = "404_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
