976e2acbcfcdaeb1f82fb27faaa2b873e901b4c1
ram1k <zo0zo0@mail.ru>
2022-10-11 20:04:58 +0300

    Added detailed_article page+changed urls,view file

--- app/mainapp/urls.py
+++ app/mainapp/urls.py
@@ -7,7 +7,8 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('page_404', views.Page404.as_view(), name='page_404'),
+    path('page_404/', views.Page404.as_view(), name='page_404'),
     path('reg_page/', views.reg_page, name='reg_page'),
     path('terms_of_service/', views.terms_of_service, name='terms_of_service'),
+    path('detailed_article/', views.DetailedArticle.as_view(), name='detailed_article')
 ]
