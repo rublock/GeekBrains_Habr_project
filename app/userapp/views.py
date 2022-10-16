b461bdc8e1ea98d63e54475826f43fe0e244fe74
Vladimir <gromovvladimir@myrambler.ru>
2022-10-16 21:13:03 +0300

    добавил вывод ошибки

--- app/userapp/views.py
+++ app/userapp/views.py
@@ -117,4 +117,3 @@ def register(request):
         register_form = MyUserRegisterForm()
     content = {'register_form': register_form}
     return render(request, 'userapp/register.html', content)
-
