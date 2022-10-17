b61343706f1dc1086d62d81f67e63578f97685c8
Vladimir <gromovvladimir@myrambler.ru>
2022-10-17 20:29:56 +0300

    попыткa 6

--- app/userapp/forms.py
+++ app/userapp/forms.py
@@ -19,18 +19,18 @@ class MyUserLoginForm(AuthenticationForm):
     def init(self, *args, **kwargs):
         super().init(*args, **kwargs)
 
-        self.fields["username"].widget.attrs.update({
-            'name': 'username',
-            'id': 'floatingInput',
-            'type': 'text',
-            'class': 'form-control rounded-4',
-        }),
-        self.fields["password"].widget.attrs.update({
-            'name': 'password',
-            'id': 'floatingPassword',
-            'type': 'password',
-            'class': 'form-control rounded-4',
-        })
+       # self.fields["username"].widget.attrs.update({
+       #     'name': 'username',
+      #      'id': 'floatingInput',
+       #     'type': 'text',
+       #     'class': 'form-control rounded-4',
+       # }),
+       # self.fields["password"].widget.attrs.update({
+       #     'name': 'password',
+       #     'id': 'floatingPassword',
+       #     'type': 'password',
+       #     'class': 'form-control rounded-4',
+       # })
 
     class Meta:
         model = User
@@ -41,30 +41,32 @@ class MyUserRegisterForm(UserCreationForm):
     def init(self, *args, **kwargs):
         super().init(*args, **kwargs)
 
-        self.fields["username"].widget.attrs.update({
-            'name': 'username',
-            'id': 'floatingInput',
-            'type': 'text',
-            'class': 'form-control rounded-4',
-        }),
-        self.fields["email"].widget.attrs.update({
-            'name': 'email',
-            'id': 'floatingInput',
-            'type': 'email',
-            'class': 'form-control rounded-4',
-        }),
-        self.fields["password1"].widget.attrs.update({
-            'name': 'password1',
-            'id': 'floatingPassword',
-            'type': 'password',
-            'class': 'form-control rounded-4',
-        }),
-        self.fields["password2"].widget.attrs.update({
-            'name': 'password2',
-            'id': 'floatingPassword',
-            'type': 'password',
-            'class': 'form-control rounded-4',
-        })
+        #self.fields["username"].widget.attrs.update({
+            #'name': 'username',
+           # 'type': 'text',
+            #'class': 'form-control rounded-3',
+           # 'id': 'floatingInput',
+            #'placeholder': 'Введите Ваш логин',
+
+       # }),
+       # self.fields["email"].widget.attrs.update({
+       #     'name': 'email',
+       #     'id': 'floatingInput',
+       #     'type': 'email',
+       #     'class': 'form-control rounded-4',
+      #  }),
+      #  self.fields["password1"].widget.attrs.update({
+      #      'name': 'password1',
+      #      'id': 'floatingPassword',
+      #      'type': 'password',
+      #      'class': 'form-control rounded-4',
+      #  }),
+       # self.fields["password2"].widget.attrs.update({
+       #     'name': 'password2',
+       #     'id': 'floatingPassword',
+       #     'type': 'password',
+       #     'class': 'form-control rounded-4',
+       # })
 
     class Meta:
         model = User
