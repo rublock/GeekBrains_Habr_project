930768eaa9f6f1037da177a10633cd8f56bf0a30
rateterin <rateterin@yandex.ru>
2022-10-29 16:53:31 +0300

    fix

--- app/mainapp/context_processors.py
+++ app/mainapp/context_processors.py
@@ -2,7 +2,7 @@ from mainapp.models import Category
 
 
 def demo_categories(request):
-    result = not (
-        "Дизайн" and "Веб-разработка" and "Мобильная разработка" and "Маркетинг"
-    ) in Category.objects.values_list("name", flat=True)
-    return {"demo_categories": result}
+    demo_categories = ("Дизайн", "Веб-разработка", "Мобильная разработка", "Маркетинг")
+    categories = Category.objects.values_list("name", flat=True)
+    result = all(dc in categories for dc in demo_categories)
+    return {"demo_categories": not result}
