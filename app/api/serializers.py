ed531c0ad5d35b296cd6a31cb9b79e39c641c68e
krlvn <92533330+krlvn@users.noreply.github.com>
2022-11-04 13:48:24 +0200

    Добавляение ID пользователя при создании новой статьи.

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,10 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from mainapp.models import Post, Comment
 
 
-class PostModelSerializer(ModelSerializer):
+class PostModelSerializer(serializers.ModelSerializer):
     # TODO: выводить список комменатриев или нет?
     # TODO: active
+    class Meta:
+        model = Post
+        fields = ("id", "category", "user", "created_at", "updated_at", "title", "description", "content", "image")
+
+
+class PostCreateModelSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = Post
         fields = ("id", "category", "user", "created_at", "updated_at", "title", "description", "content", "image")
--- app/api/views.py
+++ app/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import PostModelSerializer
+from .serializers import PostModelSerializer, PostCreateModelSerializer
 from mainapp.models import Post, Comment
 
 
 class PostViewSet(ModelViewSet):
     serializer_class = PostModelSerializer
     queryset = Post.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return PostCreateModelSerializer
+        return PostModelSerializer
