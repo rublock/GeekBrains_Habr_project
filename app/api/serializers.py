a7f59a078ba9fb72929f64dc7680fdbb4df9be2a
krlvn <92533330+krlvn@users.noreply.github.com>
2022-11-04 12:55:13 +0200

    Настроил сериализитор для модели Post

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -4,6 +4,7 @@ from mainapp.models import Post, Comment
 
 class PostModelSerializer(ModelSerializer):
     # TODO: выводить список комменатриев или нет?
+    # TODO: active
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ("id", "category", "user", "created_at", "updated_at", "status", "title", "description", "content", "image")
