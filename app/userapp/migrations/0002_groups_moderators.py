667523b74955a9ddfa7d5aeb6bc9ccb0683d1d7c
krlvn <92533330+krlvn@users.noreply.github.com>
2022-11-07 14:27:23 +0200

    Добавил группу пользователей "модераторы"

--- app/userapp/migrations/0002_groups_moderators.py
+++ app/userapp/migrations/0002_groups_moderators.py
@@ -4,7 +4,8 @@ from django.db import migrations
 
 def add_group_permissions(apps, schema_editor):
     group, created = Group.objects.get_or_create(name="moderator")
-    group.permissions.set([33,34,35,36])
+    group.permissions.set([33,34,35,36]) # Права на модерацию статей
+    group.permissions.set([25,26,27,28]) # Права на модерацию комментов
 
 
 class Migration(migrations.Migration):
