f83c8722be68137dcf422bd2f3eb992a0d3ebb17
rateterin <rateterin@yandex.ru>
2022-11-10 21:59:01 +0300

    fix

--- app/api/views.py
+++ app/api/views.py
@@ -86,5 +86,4 @@ class PostLikeAPIView(views.APIView):
         }
         serializer = PostLikesSerializer(data=data)
         serializer.is_valid(raise_exception=True)
-        data = serializer.validated_data
-        return Response(data=data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
