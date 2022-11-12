76762e5e3289d031a2b8c5aa97af92eda0f33305
mack <hypermail@yandex.ru>
2022-11-12 10:35:51 +0300

    DRY_likes_article

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -48,45 +48,39 @@ window.onclick = function (event) {
 }
 
 let protocolHost = window.location.protocol + '//' + window.location.host
-getButtonUrlDataset = document.querySelector('.likeButton').dataset.like
+getButtonUrlDataset = document.querySelectorAll('.likeButton')
 
-if (getButtonUrlDataset != "") {
-    document.querySelector('.likeButton').addEventListener('click', () => {
-        let requestURL = new URL(String(protocolHost + getButtonUrlDataset + '?format=json'));
-        const xhr = new XMLHttpRequest();
-        xhr.open
-            ('GET', requestURL, false);
-        xhr.send();
-        let likeJson = JSON.parse(xhr.response);
-        if (xhr.status === 401) {
-        return
-        } else if (likeJson.likes != 0) {
-            document.querySelector(".likeSpan").style.display = "flex";
-            document.querySelector('.likeSpan').innerHTML = likeJson.likes
-        } else if (likeJson.likes === 0) {
-            document.querySelector(".likeSpan").style.display = "none";
-        }
-    });
-    document.querySelector('.likeButtonDown').addEventListener('click', () => {
-        let requestURL = new URL(String(protocolHost + getButtonUrlDataset + '?format=json'));
-        const xhr = new XMLHttpRequest();
-        xhr.open
-            ('GET', requestURL, false);
-        xhr.send();
-        let likeJson = JSON.parse(xhr.response);
-        if (xhr.status === 401) {
-        return
-        } else if (likeJson.likes != 0) {
-            document.querySelector(".likeSpanDown").style.display = "flex";
-            document.querySelector('.likeSpanDown').innerHTML = likeJson.likes
-        } else if (likeJson.likes === 0) {
-            document.querySelector(".likeSpanDown").style.display = "none";
-        }
-    });
-}
-getCommentLikeButtonUrlDataset = document.querySelector(".likeButton.comment").dataset.like
+getButtonUrlDataset.forEach(element => {
+    if (element.dataset.like != "") {
+        element.addEventListener('click', () => {
+            let requestURL = new URL(String(protocolHost + element.dataset.like));
+            const xhr = new XMLHttpRequest();
+            xhr.open
+                ('GET', requestURL, false);
+            xhr.status
+            xhr.send();
+            let likeJson = JSON.parse(xhr.response);
+            getLikeSpanButtons = document.querySelectorAll('.likeSpan')
+            if (xhr.status === 401) {
+                return
+            } else if (likeJson.likes != 0) {
+                getLikeSpanButtons.forEach(elem => {
+                    elem.style.display = "flex";
+                    elem.innerHTML = likeJson.likes
+                })
+            } else if (likeJson.likes === 0) {
+                getLikeSpanButtons.forEach(elem => {
+                    elem.style.display = "none";
+                })
+            }
+        });
+    };
+});
+
+//Ром, лайкатеся только 1й коментарий, я знаю как пофиксить, сделаю позже.
+getCommentLikeButtonUrlDataset = document.querySelector(".likeButtonComment").dataset.like
 if (getCommentLikeButtonUrlDataset != "") {
-    document.querySelector(".likeButton.comment").addEventListener("click", () => {
+    document.querySelector(".likeButtonComment").addEventListener("click", () => {
         let requestURL = new URL(String(protocolHost + getCommentLikeButtonUrlDataset + "?format=json"));
         const xhr = new XMLHttpRequest();
         xhr.open
@@ -96,10 +90,10 @@ if (getCommentLikeButtonUrlDataset != "") {
         if (xhr.status === 401) {
         return
         } else if (likeJson.likes != 0) {
-            document.querySelector(".likeSpan.comment").style.display = "flex";
-            document.querySelector(".likeSpan.comment").innerHTML = likeJson.likes
+            document.querySelector(".likeSpanComment").style.display = "flex";
+            document.querySelector(".likeSpanComment").innerHTML = likeJson.likes
         } else if (likeJson.likes === 0) {
-            document.querySelector(".likeSpan.comment").style.display = "none";
+            document.querySelector(".likeSpanComment").style.display = "none";
         }
     });
 }
