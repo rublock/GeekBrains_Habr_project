85da6160cce84e28bf4838c08fc936054d39510f
py0trandreev <py0trandreev@yandex.ru>
2022-11-13 15:08:25 +0300

    корр.теста 007

---
+++ tests/test_urls.py
@@ -0,0 +1,15 @@
+from django.urls import reverse, resolve
+
+
+class TestUrls:
+    def test_post_detail_url(self):
+        path = reverse('mainapp:post_detail', kwargs={'post_id': 1})
+        assert resolve(path).view_name == 'mainapp:post_detail'
+        
+    def test_author_posts_url(self):
+        path = reverse('mainapp:author-posts', kwargs={'author_id': 1})
+        assert resolve(path).view_name == 'mainapp:author-posts'
+
+    def test_post_delete_url(self):
+        path = reverse('mainapp:post_delete', kwargs={'post_id': 1})
+        assert resolve(path).view_name == 'mainapp:post_delete'
---
+++ tests/test_views.py
@@ -0,0 +1,89 @@
+import django.http
+from django.http.response import HttpResponse
+
+import pytest
+from django.contrib.auth.models import AnonymousUser
+from django.contrib import auth
+from django.test import RequestFactory, TestCase
+from django.urls import reverse, reverse_lazy
+from mainapp.models import Comment, Post
+from mainapp.views import (comment_delete, post_delete, post_detail, post_edit,
+                           post_new)
+from mixer.backend.django import mixer
+from userapp.models import User
+
+# region TO_DEL
+import time
+
+from icecream import ic
+# endregion
+
+
+@pytest.mark.django_db
+class TestViews(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestViews, cls).setUpClass()
+        post_in_db = mixer.blend("mainapp.Post", id=1)
+        cls.factory = RequestFactory()
+
+    def test_new_post_for_authenticated(self):
+        strong_password = "Str#410-P@55"
+        user = User.objects.create_user(username='asdf', email='asdf@example.com', password=strong_password)
+        self.client.login(username=user.username, password=strong_password)
+        path = reverse("mainapp:post-new")
+        request = self.client.get(path)
+        request.user = user
+        
+        if isinstance(httpresp:=request.json.args[0], HttpResponse):
+            status_code = httpresp.status_code
+        
+        assert status_code == 200
+
+    def test_new_post_for_unauthenticated(self):
+        path = reverse("mainapp:post-new")
+        request = self.client.get(path)
+        request.user = AnonymousUser()
+        
+        if isinstance(httpresp:=request.json.args[0], HttpResponse):
+            status_code = httpresp.status_code
+        assert status_code == 302        
+
+
+    def test_post_detail_authenticated(self):
+        
+        path = reverse("mainapp:post_detail", kwargs={"post_id": 1})
+        request = self.factory.get(path)
+        request.user = mixer.blend(User)
+        response = post_detail(request, post_id=1)
+        assert response.status_code == 200
+
+    def test_post_detail_unauthenticated(self):
+        path = reverse("mainapp:post_detail", kwargs={"post_id": 1})
+        request = self.factory.get(path)
+        request.user = AnonymousUser()
+        response = post_detail(request, post_id=1)
+        assert response.status_code == 200
+
+    def test_new_post_unauthenticated_create(self):
+        path = reverse("mainapp:post-new")
+        request = self.factory.get(path)
+        request.user = AnonymousUser()
+        response = post_new(request)
+        assert "/users/login" in response.url
+
+    def test_post_unauthenticated_edit(self):
+        path = reverse("mainapp:post-edit", kwargs={"post_id": 1})
+        request = self.factory.get(path)
+        request.user = AnonymousUser()
+        response = post_edit(request, post_id=1)
+        assert "/users/login" in response.url
+
+    def test_post_unauthenticated_delete(self):
+        path = reverse("mainapp:post_delete", kwargs={"post_id": 1})
+        request = self.factory.get(path)
+        request.user = AnonymousUser()
+        response = post_delete(request, post_id=1)
+        assert "/users/login" in response.url
+
+    
