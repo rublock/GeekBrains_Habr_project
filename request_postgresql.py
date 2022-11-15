1cc89cb67f8809341b57b21cd0329edbf608b943
rateterin <rateterin@yandex.ru>
2022-11-15 22:50:41 +0300

    black

--- request_postgresql.py
+++ request_postgresql.py
@@ -8,14 +8,15 @@ connection = psycopg2.connect(
     password="2414",
     host="127.0.0.1",
     port="5432",
-    database="my_database")
+    database="my_database",
+)
 
 # переменная для обращения к базе данных
 cursor = connection.cursor()
 
-search_query = '''
+search_query = """
     SELECT * FROM mainapp_post
-'''
+"""
 
 cursor.execute(search_query)
 
